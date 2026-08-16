4dce724865df52d6a2aaca11f781d2fdbf457236
Thomas V Musser <tmusser@dataxu.com>
2026-08-16 19:32:04 -0400

    Route spec audits through visual default policy

--- src/chart_contract/__init__.py
+++ src/chart_contract/__init__.py
@@ -1,6 +1,7 @@
 """chart_contract public API."""
 
-from .audit import AuditFinding, AuditReport, audit_spec
+from .audit import AuditFinding, AuditReport
 from .chart import Chart
+from .spec_policy import audit_spec
 
 __all__ = ["AuditFinding", "AuditReport", "Chart", "audit_spec"]
